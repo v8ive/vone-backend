1bc6324c99ebdf1cd1c28cf881a1c9065008a9bf
v8ive <v8ive369@gmail.com>
2024-11-27 20:10:57 -0500

    Adjust Bucket Region

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-1' // Replace with your region
+    region: 'us-east-2' // Replace with your region
 });
 
 const s3 = new AWS.S3();
