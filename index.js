abd04da71887fe6f126c0cfead1cd46b57b4c253
v8ive <v8ive369@gmail.com>
2024-11-27 20:30:28 -0500

    Minor Fixes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 // Configure AWS S3
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: 'us-east-2' // Replace with your region
 });
