6369285cbe4d2a732fe7cbc0e299d014882cd902
v8ive <v8ive369@gmail.com>
2024-12-02 17:13:24 -0500

    Minor Fixes

--- routes/healthCheck.js
+++ routes/healthCheck.js
@@ -5,7 +5,11 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log('Available Routes:', router.stack);
-    res.send('Healthy');
+    res.status(200).json({
+        status: 'OK',
+        message: 'Server is healthy',
+        timestamp: new Date().toISOString()
+    });
 });
 
 module.exports = router;
